018dbfb4ada155a9678a77cc4948b1f29af31c09
Darkys <admin@darkys.ru>
2025-12-27 22:07:29 +0300

    1

--- components/stages/PasswordPromptComponent.tsx
+++ components/stages/PasswordPromptComponent.tsx
@@ -20,9 +20,9 @@ export default function TopSecret() {
         >
           <path
             stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
             d="M10 11h2v5m-2 0h4m-2.592-8.5h.01M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
           />
         </svg>
