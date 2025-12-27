5e80997e7cff74c1e0fa6305a814fad09c6126ec
Darkys <admin@darkys.ru>
2025-12-27 22:06:37 +0300

    f

--- components/stages/PasswordPromptComponent.tsx
+++ components/stages/PasswordPromptComponent.tsx
@@ -4,13 +4,32 @@ export default function TopSecret() {
   return (
     <div>
       <span>pass</span>
+
       <div
-        className="p-4 mb-4 text-sm text-fg-success-strong rounded-base bg-success-soft"
+        className="flex items-start sm:items-center p-4 mb-4 text-sm text-fg-warning rounded-base bg-warning-soft"
         role="alert"
       >
-        <span className="font-medium">Success alert!</span> The developer has
-        only one official Discord account: RUS_Darkys. All others are
-        impersonators.
+        <svg
+          className="w-4 h-4 me-2 shrink-0 mt-0.5 sm:mt-0"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M10 11h2v5m-2 0h4m-2.592-8.5h.01M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+          />
+        </svg>
+        <p>
+          The developer has only one official Discord account: RUS_Darkys. All
+          others are impersonators.
+        </p>
       </div>
     </div>
   );
